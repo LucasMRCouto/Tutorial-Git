eb1bcb80ef0f4a293425ccb4a5c90defcdfecdc7
Lucas Couto <lucascouto04@hotmail.com>
2022-08-09 11:40:48 -0300

    Adicionei mais um cout

--- Codigo1/Codigo1/src/ClassePrincial.java
+++ Codigo1/Codigo1/src/ClassePrincial.java
@@ -2,5 +2,6 @@ public class ClassePrincial {
 
     public static void main(String[] args) {
         System.out.println("Tutorial de git");
+        System.out.println("Adicionando mais uma linha");
     }
 }
